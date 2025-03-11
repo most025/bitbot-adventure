64e262ce7cd2bd2ad6b07176770374808110a43a
most025 <cristianmostazo2018@gmail.com>
2025-03-11 13:00:48 -0300

    Se ha añadido los sprite del jugador, acciones y comportamiento (v 1.0)

--- Scene/main/player.gd
+++ Scene/main/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -500.0
 var gravity=800.0
 
 func _physics_process(delta: float) -> void:
@@ -24,3 +24,21 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	# Añadiendo direccion de movimiento del sprite.
+	# direction sprite
+	if direction == 1:
+		# si el jugador mira a la derecha se ejecuta 
+		$AnimatedSprite2D.flip_h=false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h=true
+	
+	# Añadiendo deciones de acciones en el jugador 
+	if not is_on_floor():
+		#si el jugador no esta en el suelo se ejecuta este frame. 
+		$AnimatedSprite2D.play("jump")
+	elif direction != 0:
+		# si el jugador se ha movido se ejecutara este frame.
+		$AnimatedSprite2D.play("walk")
+	else:
+		# si el jugador no se ha movido se ejecutara este frame.
+		$AnimatedSprite2D.play("idle")
